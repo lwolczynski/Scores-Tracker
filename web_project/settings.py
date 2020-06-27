de2767bf74515c07b88a3f51a091662721337dd8
unknown <lwolczynski7@gmail.com>
2020-06-27 10:49:58 -0500

    Settings modified

--- web_project/settings.py
+++ web_project/settings.py
@@ -130,5 +130,6 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
