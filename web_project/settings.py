726a3e7627cefc6bcf6d375690418f5c96af1681
unknown <lwolczynski7@gmail.com>
2020-08-07 10:10:31 -0500

    New hostname add + minor changes

--- web_project/settings.py
+++ web_project/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'scores-tracker.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'scores-tracker.herokuapp.com', 'myscore.golf']
 
 
 # Application definition
